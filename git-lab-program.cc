4514dc0f85e98bf78bb35f4f8251e6fb8efe824e
Sarah Ivey <si102623@ohio.edu>
2025-01-21 11:34:38 -0500

    Initial commit4

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sarah Ivey
+ *        Date: 1/21/2025
+ * Description: Program for Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
